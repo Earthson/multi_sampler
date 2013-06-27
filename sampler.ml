c5355c0cdcd73509b2bd7f3a23fed0295397d5ce
Earthson Lu <Earthson.Lu@gmail.com>
2013-06-27 18:24:58 +0800

    update

--- sampler.ml
+++ sampler.ml
@@ -96,6 +96,7 @@ let multi_sampler k f =
     in let show_vals () = avals
     in sample_gen, set, to_set, update_with_stack, clear, show_sums, show_stats, show_vals;;
 
-(*testing*)
+(*testing
 let test_f = function i -> (float_of_int (i+1))*.0.3
 let gen, set, to_set, update_with_stack, clear, show_sums, show_stats, show_vals = multi_sampler 10 test_f;;
+*)
